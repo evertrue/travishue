10eddb6f82ee518074257525985a1aa9508d21db
PJ Gray <pj@evertrue.com>
2014-01-16 16:36:12 -0500

    Added da codes

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# travishue
+        
+### help
+    
+   th
+
+  Monitor travis builds with Hue
+
+  Commands:
+    help                 Display global or [command] help documentation.
+    travis:token         Set Travis pro token 
+    travis:watch         Watch Travis builds
+
+  Global Options:
+    -h, --help           Display help documentation 
+    -v, --version        Display version information 
+    -t, --trace          Display backtrace when an error occurs 
+  
+  Author:
+    PJ Gray <pj@evertrue.com>
---
+++ lib/travishue.rb
@@ -0,0 +1,59 @@
+require 'keychain'
+require 'travis/pro'
+
+command :'travis:token' do |c|
+  c.syntax = 'travis:token TOKEN [...]'
+  c.summary = 'Set Travis pro token '
+
+  c.action do |args, options|
+    say_error "Missing arguments, expected [TOKEN] (travis token --pro)" and abort if args.nil? or args.empty? or args.count < 1
+
+	Keychain.generic_passwords.create(
+		:service => 'travishue', 
+		:password => args[0], 
+		:account => "protoken")
+
+	puts "Added Token"
+
+  end
+end
+
+command :'travis:watch' do |c|
+  c.syntax = 'travis:watch'
+  c.summary = 'Watch Travis builds'
+
+  c.action do |args, options|
+	token = Keychain.generic_passwords.where(:service => 'travishue').where(:account => 'protoken').first
+
+	Travis::Pro.access_token = token
+	user = Travis::Pro::User.current
+
+	puts "Hello #{user.name}: watching your builds."
+
+	Travis::Pro.listen do |stream|
+	  stream.on('build:started', 'build:finished') do |event|
+	    puts "#{event.repository.slug} just #{event.build.state}"
+	    if event.build.state == 'failed'
+	    	%x(hue 1 failed)
+	    	%x(hue 2 failed)
+	    end
+
+	    if event.build.state == 'errored'
+	    	%x(hue 1 failed)
+	        %x(hue 2 failed)
+	    end
+
+	    if event.build.state == 'passed'
+	    	%x(hue 1 passed)
+	    	%x(hue 2 passed)
+	    end
+
+	    if event.build.state == 'started'
+	    	%x(hue 1 started)
+	    	%x(hue 2 started)
+	    end
+	  end
+	end
+  end
+end
+
